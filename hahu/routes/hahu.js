4379cd29cf54e332f61caec522065c565b3859ea
harmatosbikafing <scheuer.patrik@gmail.com>
2022-03-23 10:18:37 +0100

    Changing the code

--- hahu/routes/hahu.js
+++ hahu/routes/hahu.js
@@ -1,3 +1,4 @@
+const { json } = require('express');
 const express = require('express');
 const router = express.Router();
 const Hirdetes = require('../models/hirdetes');
@@ -12,14 +13,23 @@ router.post('/', function(req, res, next) {
     const arFt = req.body.arFt;
     const kepUrl = req.body.kepUrl;
 
-    const hirdetes = new Hirdetes({_id, kategoria, cim, leiras, hirdetesDatuma,
-    serulesmentes, arFt, kepUrl});
-    hirdetes
-    .save()
-    .then(res.json({
-        "message" : "A rekord rögzítése sikeres."
-    }))
-    .catch(err => console.log(err))
+    try {
+        if (arFt % 1000 != 0) {
+            throw Error("Az ár nem osztható 1000-el!")
+        }
+        const hirdetes = new Hirdetes({_id, kategoria, cim, leiras, hirdetesDatuma,
+            serulesmentes, arFt, kepUrl});
+            hirdetes
+            .save()
+            .then(res.status(200).json({
+                "message" : "A rekord rögzítése sikeres."
+            }))
+            .catch(err => console.log(err))
+    } catch (err) {
+        res.status(400).json({
+            "arFt" : err.message
+        })
+    }
 })
 
 module.exports = router;
